359e3fd269f7efc68a61290d44610a3f9dc09c56
Cristian Vergel <cristianvergel5@gmail.com>
2018-04-07 21:31:22 -0500

    insertar imagenes y textos dentro detro de ventana, parte grafica

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+ventana = Tk()
+ventana.title("nueva ventana")
+ventana.geometry("900x500")
+ventana.config(background="dark turquoise")
+etiqueta = Label(ventana, text="Automatas", bg="gold", fg="black")
+etiqueta.pack()
+imagenAuto = PhotoImage(file="barCode.png")
+lbImage = Label(ventana, image=imagenAuto)
+lbImage.pack()
+ventana.mainloop()
