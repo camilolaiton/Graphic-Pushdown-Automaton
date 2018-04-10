a94228e10703a5a875f31316600ae22f68d4ce4d
Camilo Laiton <kmilo9713.cl@gmail.com>
2018-04-09 21:03:57 -0500

    proceso pila grafica, corregir el bug del timer

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from tkinter import *
 from pila import Pila 
+from pushDownAutomaton import PDA
 import time
 
-def pintarPila(coord, mensaje):
-	
+def pintarPila(coord, mensaje):	
 	i = canvas.create_rectangle(coord, width=5, fill='red', activefill="#0017F9")
 	m = canvas.create_text (coord[2]-80, coord[1]+20, text = mensaje, activefill="blue")
 	return i, m
@@ -13,11 +13,11 @@ def borrarPila(i, m):
 	canvas.delete(i)
 
 class pilaGrafica:
-	def __init__(self, coord):
+	def __init__(self, coord, proceso):
 		self.coord = coord
 		self.lista = []
 		self.mensajes = []
-		self.aumentarPila("#")
+		self.proceso = proceso
 
 	def aumentarPila(self, mensaje):
 		
@@ -32,6 +32,18 @@ class pilaGrafica:
 		if len(self.lista) > 1:
 			self.coord = self.coord[0], self.coord[1]+50, self.coord[2], self.coord[3]+50
 			borrarPila(self.lista.pop(), self.mensajes.pop())
+
+	def dibujarPila(self):
+
+		for w in self.proceso:
+			if w[1] == 1:
+				print("mete")
+				self.aumentarPila(w[0])
+			else:
+				print("saco")
+				self.decrementarPila()
+
+			#time.sleep(1)
 
 main_window = Tk()
 main_window.title("PushDown Automaton")
@@ -64,7 +76,25 @@ canvas.pack(expand=YES, fill=BOTH)
 label3 = Label(main_window, bg="dark turquoise")
 label3.grid(row=1, column=2, sticky="nsew")
 
-pila = pilaGrafica(coord)
+automata1 = PDA("p", "r", "#")
+#  Transiciones grafo 1 --- REGLAS CON ESTE AUTOMATA YA ESTA BUENO
+
+label_PtoP = ["p-p","b/b/bb", "a/b/ba", "b/a/ab", "a/a/aa", "b/#/#b", "a/#/#a"]
+label_PtoQ = ["p-q","c/#/#", "c/b/b", "c/a/a"]
+label_QtoQ = ["q-q","b/b/λ", "a/a/λ"]
+label_QtoR = ["q-r","λ/#/#"]
+
+edges1 = [label_PtoQ, label_PtoP, label_QtoR, label_QtoQ]
+automata1.setEdges(edges1)
+
+print(automata1.evaluarCadena("abbcbba", automata1.estadoInicial))
+print("Proceso de la pila donde 1 es meter y 0 es sacar: \n", automata1.proceso)
+
+pila = pilaGrafica(coord, automata1.proceso)
+
+Comenzar = Button(label3, text="Comenzar proceso", command=pila.dibujarPila)
+Comenzar.pack(expand=False, fill=BOTH)
+
 """
 AumentarPila = Button(label3, text="Aumentar pila", command=pila.aumentarPila)
 AumentarPila.pack(expand=False, fill=BOTH)
